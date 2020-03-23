2b9f673709122956935250a24ecfd5f44786c574
Khalil Crumpler <crumplerkhalil@gmail.com>
2020-03-23 09:50:18 -0400

    first commit

---
+++ src/main/java/Food.java
@@ -0,0 +1,8 @@
+public class Food {
+    // every item has a name price type and expiration
+
+    private String name;
+    private String price;
+    private String type;
+    private String expiration;
+}
